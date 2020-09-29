684998cf62f28f561c690ff011993224287873e6
Caique Moraes <ike.moraes93@gmail.com>
2020-09-29 09:47:36 -0300

    Updates

---
+++ precache-manifest.87f06b14087991a33c9ac8fadf1956db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8bee6d3712610842de5f504040fe27",
+    "url": "/brafe-react/index.html"
+  },
+  {
+    "revision": "797daf62f885298d7e25",
+    "url": "/brafe-react/static/js/2.bb722003.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brafe-react/static/js/2.bb722003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b99a726b3ef7b6a04de",
+    "url": "/brafe-react/static/js/main.89f5de04.chunk.js"
+  },
+  {
+    "revision": "8ab097b2501c22629530",
+    "url": "/brafe-react/static/js/runtime-main.3972a421.js"
+  },
+  {
+    "revision": "76353a2137ae63b0ee546218e076da8f",
+    "url": "/brafe-react/static/media/bg-intro.76353a21.jpg"
+  },
+  {
+    "revision": "9ed21e65a7d35a86610768c5d36d7988",
+    "url": "/brafe-react/static/media/botafogo.9ed21e65.jpg"
+  },
+  {
+    "revision": "a4342daa18b36d9b3b091b95ef7b74fb",
+    "url": "/brafe-react/static/media/cafe-1.a4342daa.jpg"
+  },
+  {
+    "revision": "5dfdd83104ef1eaf1adc071d92994e5f",
+    "url": "/brafe-react/static/media/cafe-2.5dfdd831.jpg"
+  },
+  {
+    "revision": "a3855b88bdf5746201499162051a93e6",
+    "url": "/brafe-react/static/media/iguatemi.a3855b88.jpg"
+  },
+  {
+    "revision": "5b1c615e20e5910f6034bd329640f1ac",
+    "url": "/brafe-react/static/media/mineirao.5b1c615e.jpg"
+  }
+]);
